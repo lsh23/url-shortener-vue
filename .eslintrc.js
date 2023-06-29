1989cbf3af14b323d2c8ccba96c1b03eb6bc0581
lsh23 <shseoul14@gmail.com>
2023-06-29 18:10:23 +0900

    chore : prettier rules 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,18 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ['error', {
+      singleQuote: true,
+      semi: true,
+      useTabs: true,
+      tabWidth: 2,
+      trailingComma: 'all',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'avoid',
+    }] 
+    // prettier의 설정이 eslint와 충돌할 수 있으므로 eslint에 prettier 규칙을 넣어준다. 
+    //또한 코드 레벨로 규칙을 관리하면 개발툴이 다르더라도 동일한 규칙으로 통일 할 수 있다.
   },
   overrides: [
     {
