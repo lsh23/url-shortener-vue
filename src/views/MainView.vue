f72617828e0bd6651c1dc5c7bede281d756ad15f
lsh23 <shseoul14@gmail.com>
2023-07-04 13:13:00 +0900

    feat : main view 페이지에 대한 1차 디자인을 구성한다.

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,11 +1,20 @@
 <template>
-	<div>main</div>
+	<div>
+		<Logo></Logo>
+		<UrlShortener></UrlShortener>
+		<SimpleUrlList></SimpleUrlList>
+	</div>
 </template>
 
 <script>
+import Logo from '@/components/common/Logo.vue';
+import SimpleUrlList from '@/components/SimpleUrlList.vue';
+import UrlShortener from '@/components/UrlShortener.vue';
+
 export default {
 	name: 'MainView',
+	components: { Logo, UrlShortener, SimpleUrlList },
 };
 </script>
 
-<style></style>
+<style scoped></style>
