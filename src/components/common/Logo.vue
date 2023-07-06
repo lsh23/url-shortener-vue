dc8532e6dccf2b1e0d32c17969545dcb2a30394e
lsh23 <shseoul14@gmail.com>
2023-07-06 13:24:33 +0900

    feat : 사이즈 %를 입력 받아서 크기를 조절 할 수 있다.

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -1,12 +1,29 @@
 <template>
 	<div class="logo">
-		<img src="@/assets/logo.png" alt="" />
+		<img src="@/assets/logo.png" alt="" :style="logoSize" />
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'Logo',
+	props: {
+		size: {
+			type: Number,
+			required: false,
+		},
+	},
+	computed: {
+		logoSize() {
+			if (this.size === undefined) {
+				return {};
+			}
+			console.log('?');
+			return {
+				width: `${this.size}%`,
+			};
+		},
+	},
 };
 </script>
 
